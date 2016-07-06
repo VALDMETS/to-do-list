38982785f6afd6d27da9f03761fce486c6e8d7e2
Ben Valdmets-Harris <benvaldmets@gmail.com>
2016-07-06 13:34:33 -0500

    got a nice toggle for done items

--- main.js
+++ main.js
@@ -17,23 +17,29 @@ function getAllItems (response) {
 }
 
 function divBuilder (item, i, arr) {
-  if (item.complete === false) {
-    $('.mainview').append('<div class="todoitem"><input type="button" data-id="' + item._id + '" value=""><h5>' + item.title + '</h5><p>' + item.details + '</p></div>');
-  }
+  // if (item.complete === "false") {
+    $('.mainview').append('<div class="todoitem"><input type="button" data-id="' + item._id + '" data-done="' + item.complete + '" value=""><h5>' + item.title + '</h5><p>' + item.details + '</p></div>');
+  // }
 }
 
 function completeClick (evt) {
-  // console.log($(this.dataset.DOMStringMap));
   console.log(evt.target.dataset.id);
-  $.ajax({
+  var currentTarget = {
     url: 'http://tiny-za-server.herokuapp.com/collections/benstodo/' + evt.target.dataset.id,
     type: 'PUT',
     dataType: 'json',
-    data: {
-      complete: true
-    },
     success: function (){
       console.log('i did it!');
     }
-  });
+  };
+  if (evt.target.dataset.done === "true") {
+      currentTarget = Object.assign(currentTarget, {data: {complete: false}});
+      evt.target.dataset.done = "false";
+      console.log('made false');
+  } else if (evt.target.dataset.done === "false") {
+      currentTarget = Object.assign(currentTarget, {data: {complete: true}});
+      console.log('made true');
+      evt.target.dataset.done = "true";
+  }
+  $.ajax(currentTarget);
 }
